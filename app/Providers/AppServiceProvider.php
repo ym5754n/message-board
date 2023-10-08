0d9add4c3584359f5048eea3bdef2caba7637f51
ymstsun <29919765+ymstsun@users.noreply.github.com>
2023-10-08 14:54:49 +0900

    add boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        \Schema::defaultStringLength(191);
+        \URL::forceScheme('https');
     }
 }
