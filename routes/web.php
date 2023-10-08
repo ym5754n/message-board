2ff02ef1f97065c75c834d07f6b6dc6bc0decbd7
ymstsun <29919765+ymstsun@users.noreply.github.com>
2023-10-08 15:15:05 +0900

    add routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// root
+Route::get('/', 'MessageController@index');
+
+// 参照
+Route::get('messages', 'MessageController@index')->name('messages.index');
+Route::get('messages/create', 'MessageController@create')->name('messages.create');
+Route::get('message/{id}/edit', 'MessageController@edit')->name('messages.edit');
+
+// CRUD
+Route::get('messages/{id}', 'MessageController@show');
+Route::post('messages', 'MessageController@store');
+Route::put('messages/{id}', 'MessageController@update');
+Route::delete('messages/{id}', 'MessageController@destroy');
