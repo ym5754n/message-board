9b3f7ac798ac1de9ee624f01acfae07644972ffe
ymstsun <29919765+ymstsun@users.noreply.github.com>
2023-10-14 15:53:19 +0900

    add messages seeder

---
+++ database/seeders/MessagesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MessagesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        for ($i = 0; $i < 100; $i++) {
+            DB::table('messages')->insert([
+                'title' => 'title ' . $i,
+                'content' => 'content ' . $i,
+            ]);
+        }
+    }
+}
